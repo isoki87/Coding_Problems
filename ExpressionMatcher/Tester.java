0d92a8f9ac1916bc2f67986eca978ff88654985d
isoki87 <helloleolin@gmail.com>
2018-08-01 16:17:37 -0700

    Add files via upload

---
+++ ExpressionMatcher/Tester.java
@@ -0,0 +1,12 @@
+package com.leo.interview;
+
+public class Tester {
+	
+	
+	public static void main(String[] args){
+		ExpressionMatcher t = new ExpressionMatcher();
+		System.out.println(t.isMatch("abk", ".*b"));
+	}
+}
+
+
